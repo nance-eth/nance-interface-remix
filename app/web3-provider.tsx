ecc9414e5a20867e271d4bebd22580ae1433f5cf
twodam <twodam@protonmail.com>
2024-03-14 11:34:21 +0800

    Revert "Revert "feat(main): merge""
    This reverts commit ad8be45791f59f23bd9320442052ecf57d7b3817.

--- app/web3-provider.tsx
+++ app/web3-provider.tsx
@@ -1,36 +1,17 @@
-import { WagmiProvider, createConfig, http } from "wagmi";
-import { mainnet } from "wagmi/chains";
+import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import { ConnectKitProvider } from "connectkit";
 import { PropsWithChildren } from "react";
-
-const queryClient = new QueryClient();
+import { getWagmiConfig } from "./utils/config";
 
 export function Web3Provider({
   children,
   wcProjectId,
-}: PropsWithChildren<{ wcProjectId: string | undefined }>) {
-  const defaultConfig = getDefaultConfig({
-    // Your dApps chains
-    chains: [mainnet],
-    transports: {
-      // RPC URL for each chain
-      [mainnet.id]: http(mainnet.rpcUrls.default.http[0]),
-    },
-
-    // Required API Keys
-    walletConnectProjectId: wcProjectId || "",
-
-    // Required App Info
-    appName: "Nance.app",
-
-    // Optional App Info
-    appDescription: "Your App Description",
-    appUrl: "https://nance.app",
-    appIcon: "https://nance.app/favicon.ico",
-  });
-  defaultConfig.ssr = true;
-  const config = createConfig(defaultConfig);
+}: PropsWithChildren<{
+  wcProjectId: string | undefined;
+}>) {
+  const config = getWagmiConfig(wcProjectId);
+  const queryClient = new QueryClient();
 
   return (
     <WagmiProvider config={config}>
