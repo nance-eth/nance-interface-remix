754560ca3db7f426c1fec4f97881bf90537b8bbb
twodam <twodam@protonmail.com>
2024-03-24 10:12:11 +0800

    perf: load spaces data from browser so user won't wait too long to see the page

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,9 +1,8 @@
-import { LoaderFunctionArgs, json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import HeroSection, { SimpleSpaceEntry } from "./hero-section";
 import { getAllSpaces } from "@nance/nance-sdk";
 
-export const loader = async ({ request }: LoaderFunctionArgs) => {
+export async function clientLoader() {
   const spaces = await getAllSpaces();
   const top4Spaces: SimpleSpaceEntry[] = spaces
     // filter test spaces
@@ -20,11 +19,30 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
       };
     });
 
-  return json({ spaces: top4Spaces });
-};
+  return { spaces: top4Spaces };
+}
+clientLoader.hydrate = true;
+
+export function HydrateFallback() {
+  const spaces = [
+    { id: "juicebox", name: "juicebox", snapshotSpace: "jbdao.eth" },
+    {
+      id: "daosquare",
+      name: "daosquare",
+      snapshotSpace: "community.daosquare.eth",
+    },
+    { id: "moondao", name: "moondao", snapshotSpace: "tomoondao.eth" },
+    {
+      id: "thirstythirsty",
+      name: "thirstythirsty",
+      snapshotSpace: "gov.thirstythirsty.eth",
+    },
+  ];
+  return <HeroSection top4Spaces={spaces} />;
+}
 
 export default function Index() {
-  const { spaces } = useLoaderData<typeof loader>();
+  const { spaces } = useLoaderData<typeof clientLoader>();
 
   return <HeroSection top4Spaces={spaces} />;
 }
